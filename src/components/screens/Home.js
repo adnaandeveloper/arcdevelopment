424e613851dd032f2c6f584a9f58acefe77192ae
Adnaan Mohamud <amaydeveloper@gmail.com>
2022-02-28 14:10:46 +0100

    the service bloc is full responsive now and implemented

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -6,9 +6,11 @@ import {
   Grid,
   Button,
   Typography,
+  useMediaQuery,
 } from '@material-ui/core/'
 import animationData from '../../animations/landinganimation/data'
 import ButtonArrow from '../ui/ButtonArrow'
+import customoftwareIcon from '../../assets/Custom Software Icon.svg'
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -36,18 +38,20 @@ const useStyles = makeStyles((theme) => ({
   },
 
   learnMoreButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: 'none',
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
+    ...theme.typography.learnButton,
     fontSize: '0.9rem',
     height: 45,
     width: 145,
   },
-
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sx')]: {
+      marginBottom: '2em',
+    },
+  },
   mainContainer: {
     marginTop: '5em',
     [theme.breakpoints.down('md')]: {
@@ -64,11 +68,32 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: 0,
     },
   },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange,
+  },
+
+  subtitle: {
+    marginBottom: '1em',
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25,
+    },
+  },
 }))
 
 export default function Home() {
   const classes = useStyles()
   const theme = useTheme()
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'))
 
   const defaultOptions = {
     loop: true,
@@ -82,6 +107,7 @@ export default function Home() {
   return (
     <Grid container direction='column' className={classes.mainContainer}>
       <Grid item>
+        {/*------- Hero Block.......*/}
         <Grid container justify='flex-end' alignItems='center' direction='row'>
           <Grid sm item className={classes.heroTextContainer}>
             <Typography variant='h2' align='center'>
@@ -112,6 +138,50 @@ export default function Home() {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie loop animationData={animationData} play />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item>
+        {' '}
+        {/*-----Services Block......*/}
+        <Grid
+          container
+          direction='row'
+          className={classes.serviceContainer}
+          justify={matchesSM ? 'center' : undefined}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>Custom Software Development</Typography>
+            <Typography variant='subtitle1'>
+              Save Energi. Save Time. Save Money.
+            </Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Complete digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button variant='outlined' className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More </span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              ></ButtonArrow>
+            </Button>
+          </Grid>
+
+          <Grid item>
+            <img
+              className={classes.icon}
+              alt='custom software Icon'
+              src={customoftwareIcon}
+            />
           </Grid>
         </Grid>
       </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,6 +1,7 @@
 import { createTheme } from '@material-ui/core/styles'
 const arcBlue = '#0B72B9'
 const arcOrange = '#FFBA60'
+const arcGrey = '#868686'
 
 const theme = createTheme({
   palette: {
@@ -35,6 +36,32 @@ const theme = createTheme({
       fontSize: '2.5rem',
       color: `${arcBlue}`,
       lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: 'Pacifico',
+      fontSize: '2.5rem',
+      color: arcBlue,
+    },
+
+    h4: {
+      fontFamily: 'Raleway',
+      color: arcBlue,
+      fontSize: '1.75rem',
+      fontWeight: 700,
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      color: `${arcGrey}`,
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      borderWidth: 2,
+      textTransform: 'none',
+      color: arcBlue,
+      borderRadius: 50,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
     },
   },
 })
